f48b5573d3cf577ffa35373b64af6e411afb9465
Ilya Dudikov <ilyadud@mail.ru>
2021-05-03 13:19:24 +0700

    lesson #11

--- database/seeders/QuizResultSeeder.php
+++ database/seeders/QuizResultSeeder.php
@@ -64,6 +64,11 @@ class QuizResultSeeder extends Seeder
                 'user_id' => 2,
                 'lesson_id' => 10,
                 'correct_percentage' => '100'
+            ],
+            [
+                'user_id' => 2,
+                'lesson_id' => 11,
+                'correct_percentage' => '100'
             ]
         ]);
     }
